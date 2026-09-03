93f356478f1c5427bb2e136caf855a68ef03507b
deadend-g4mes <leighton.h.carpenter@gmail.com>
2026-09-03 00:21:44 -0800

    now it works

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-document.getElementById("download").addEventListener('click', function(){
+function download(){
     const file = "files/Leighton_Carpenter_Resume.pdf";
 
     const link = document.createElement('a');
-    
+
     link.href = file;
     link.download = 'Leighton_Carpenter_Resume.pdf'
 
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  const downloadBtn = document.querySelector('#download');
+  downloadBtn?.addEventListener('click', download, false);
 });
